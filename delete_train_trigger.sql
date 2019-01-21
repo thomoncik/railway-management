985e99fdffaca41f1ac43a139a528e6e3b36c17e
mdymek <mr.dymek@student.uj.edu.pl>
2019-01-21 22:39:57 +0100

    Changed

--- delete_train_trigger.sql
+++
@@ -1,9 +0,0 @@
-IF OBJECT_ID('delete_train', 'TR') IS NOT NULL DROP TRIGGER delete_train GO
-
-CREATE TRIGGER delete_train
-  ON train
-  FOR DELETE
-  AS
-  DELETE
-  FROM train_station
-  WHERE EXISTS ( SELECT D.* FROM deleted as D WHERE D.id = train_id AND D.name = train_name )
