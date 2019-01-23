f0d02c01e14af29a20206dc248854db4bfa4c168
hjaremko <hjaremko@outlook.com>
2019-01-23 15:53:31 +0100

    Change station_id and track_id type back to int

--- new-train-procedure.sql
+++ new-train-procedure.sql
@@ -1,55 +1,62 @@
-IF OBJECT_ID ('new_train', 'P') IS NOT NULL DROP PROC new_train
+IF OBJECT_ID('new_train', 'P') IS NOT NULL DROP PROC new_train
 GO
 
-CREATE PROC new_train (
-    @train_id NVARCHAR(16),
-    @train_name NVARCHAR(256),
-    @carrier_name NVARCHAR(256)
-)
+CREATE PROC new_train(@train_id NVARCHAR(16), @train_name NVARCHAR(256), @carrier_name NVARCHAR(256))
 AS
 
 DECLARE @carrier_id INT
 
-IF NOT EXISTS (SELECT C.name FROM carrier AS C WHERE C.name = @carrier_name)
-BEGIN
-    RAISERROR ('Given Carrier %s does not exist!!', 16, 1, @carrier_name)
-    RETURN(1)
-END
+IF NOT EXISTS(SELECT c.name
+              FROM carrier AS c
+              WHERE c.name = @carrier_name)
+    BEGIN
+        RAISERROR ('Given Carrier %s does not exist!!', 16, 1, @carrier_name)
+        RETURN (1)
+    END
 
-SET @carrier_id = (SELECT C.id FROM carrier AS C WHERE C.name = @carrier_name)
+SET @carrier_id = (SELECT c.id
+                   FROM carrier AS c
+                   WHERE c.name = @carrier_name)
 
-IF EXISTS (SELECT * FROM train AS T WHERE T.id = @train_id AND T.name = @train_name)
-BEGIN
-    RAISERROR ('Given train %s %s already exists!!', 16, 1, @train_id, @train_name)
-    RETURN(2)
-END
+IF EXISTS(SELECT *
+          FROM train AS t
+          WHERE t.id = @train_id
+            AND t.name = @train_name)
+    BEGIN
+        RAISERROR ('Given train %s %s already exists!!', 16, 1, @train_id, @train_name)
+        RETURN (2)
+    END
 
 DECLARE @departure_station_id INT
 SET @departure_station_id = CONVERT(INT, SUBSTRING(@train_id, 1, 1))
 
-IF NOT EXISTS (SELECT S.id FROM station AS S WHERE S.id = @departure_station_id)
-BEGIN
-    RAISERROR ('Departure station id %d does not exist!', 16, 1, @departure_station_id)
-    RETURN(3)
-END
+IF NOT EXISTS(SELECT s.id
+              FROM station AS s
+              WHERE s.id = @departure_station_id)
+    BEGIN
+        RAISERROR ('Departure station id %d does not exist!', 16, 1, @departure_station_id)
+        RETURN (3)
+    END
 
 DECLARE @arrival_station_id INT
 SET @arrival_station_id = CONVERT(INT, SUBSTRING(@train_id, 2, 1))
 
-IF NOT EXISTS (SELECT S.id FROM station AS S WHERE S.id = @arrival_station_id)
-BEGIN
-    RAISERROR ('Arrival station id %d does not exist!', 16, 1, @arrival_station_id)
-    RETURN(4)
-END
+IF NOT EXISTS(SELECT s.id
+              FROM station AS s
+              WHERE s.id = @arrival_station_id)
+    BEGIN
+        RAISERROR ('Arrival station id %d does not exist!', 16, 1, @arrival_station_id)
+        RETURN (4)
+    END
 
 IF @departure_station_id = @arrival_station_id
-BEGIN
-    RAISERROR ('Departure station id and arrival station id cannot be the same!', 16, 1)
-    RETURN(5)
-END
+    BEGIN
+        RAISERROR ('Departure station id and arrival station id cannot be the same!', 16, 1)
+        RETURN (5)
+    END
 
 INSERT INTO train
-    VALUES (@train_id, @train_name, @carrier_id)
+VALUES (@train_id, @train_name, @carrier_id)
 
-RETURN(0)
+RETURN (0)
 GO
