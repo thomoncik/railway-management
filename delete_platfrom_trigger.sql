bf0e3e776fd120d03640bdbfc7dd6a904fe26e73
Tomasz Homoncik <tjhomoncik@gmail.com>
2019-01-23 12:04:54 +0100

    Remove unnessesery files

--- delete_platfrom_trigger.sql
+++
@@ -1,9 +0,0 @@
-IF OBJECT_ID('delete_platform', 'TR') IS NOT NULL DROP TRIGGER delete_platform GO
-
-CREATE TRIGGER delete_platform
-  ON platform
-  FOR DELETE
-  AS
-  DELETE
-  FROM track
-  WHERE platform_id IN (SELECT D.id FROM deleted as D)
