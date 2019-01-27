c98939636c6c1504ac0568004b8c03bd0338df62
hjaremko <hjaremko@outlook.com>
2019-01-27 13:02:17 +0100

    Add ticket_price attribute to the carriers table
    Close #46
    Close #47

--- insert-carrier-trigger.sql
+++ insert-carrier-trigger.sql
@@ -11,22 +11,30 @@ BEGIN
 
     DECLARE @carrier_id INT
     DECLARE @carrier_name NVARCHAR(256)
+    DECLARE @carrier_ticket MONEY
 
     OPEN to_insert
 
-    FETCH to_insert INTO @carrier_id, @carrier_name
+    FETCH to_insert INTO @carrier_id, @carrier_name, @carrier_ticket
 
     WHILE @@FETCH_STATUS = 0
     BEGIN
-        SET @carrier_name = UPPER(@carrier_name)
-        INSERT INTO carrier
-        VALUES (@carrier_id, @carrier_name)
+        IF @carrier_ticket >= 0
+        BEGIN
+            SET @carrier_name = UPPER(@carrier_name)
 
-        FETCH to_insert INTO @carrier_id, @carrier_name
+            INSERT INTO carrier
+            VALUES (@carrier_id, @carrier_name, @carrier_ticket)
+
+            FETCH to_insert INTO @carrier_id, @carrier_name, @carrier_ticket
+        END ELSE
+        BEGIN
+            RAISERROR ('Ticket price must be non-negative!', 16, 1)
+            RETURN
+        END
     END
 
     CLOSE to_insert
     DEALLOCATE to_insert
 END
-
 GO
